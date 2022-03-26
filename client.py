c9fded86cd646ff21eb4762eb4140d4583210fc3
Taylan Sahin <m.taylansahin99@gmail.com>
2022-03-26 19:06:21 +0300

    Get parameters in URL

--- client.py
+++ client.py
@@ -2,6 +2,6 @@ import requests
 
 BASE = "http://localhost:5000/"
 
-response = requests.get(BASE + 'helloworld')
+response = requests.get(BASE + 'helloworld/testname/20')
 
 print(response.json())
--- main_app.py
+++ main_app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
-    def get(self):
-        return {"data": "hello world"}
+    def get(self, name, age):
+        return {"name": name, "age": age}
 
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:age>")
 
 if __name__=='__main__':
     app.run(debug=True)
