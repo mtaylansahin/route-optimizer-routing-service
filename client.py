da0533d945c94d97dd7fd10ceba884a0fe98ed12
Taylan Sahin <m.taylansahin99@gmail.com>
2022-03-26 19:36:27 +0300

    Added PUT and DELETE methods

--- client.py
+++ client.py
@@ -2,6 +2,14 @@ import requests
 
 BASE = "http://localhost:5000/"
 
-response = requests.get(BASE + 'helloworld/testname/20')
+response = requests.put(BASE + 'video/1', {'name':'first video', 'views':20, 'like':5})
+print(response.json())
 
+response = requests.get(BASE + 'video/1')
+print(response.json())
+
+response = requests.delete(BASE + 'video/1')
+print(response)
+
+response = requests.get(BASE + 'video/1')
 print(response.json())
